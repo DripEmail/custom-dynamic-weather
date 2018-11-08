194b386812f1205fe7eb6a604bf658733a7ffeb5
William Johnston <william.johnston@drip.com>
2018-11-08 10:28:59 -0600

    Clarify required environment variable

--- main.go
+++ main.go
@@ -100,6 +100,9 @@ func zipHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	if os.Getenv("DARK_SKY_API_KEY") == "" {
+		log.Fatal("DARK_SKY_API_KEY environment variable is required. Get one at https://darksky.net/dev/account")
+	}
 	http.HandleFunc("/api", zipHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
